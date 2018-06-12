2997785127cb90e097ed250221f5df752608523c
leereek@gmail.com <leereek@gmail.com>
2018-06-12 23:16:27 +0200

    build/clean and deploy wheel

---
+++ build.sh
@@ -0,0 +1,18 @@
+#clean previous build
+rm -rf build fibonacci_pkg dist *.egg-info
+rm -f *.pyd
+
+if [[ $* == --clean ]]; then
+    exit 0
+fi
+
+#build extension
+python setup_ext.py build_ext --inplace
+
+#create module
+mkdir --parents ./fibonacci_pkg
+mv fibonacci.*.pyd ./fibonacci_pkg
+touch ./fibonacci_pkg/__init__.py
+
+#dist wheel
+python setup_dist.py sdist bdist_wheel
---
+++ setup_dist.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="fibonacci",
+    version="0.0.1",
+    author="Piotr Markiewicz",
+    description="Yet another fib Nth nubmer implementation",
+    packages=setuptools.find_packages()
+)
