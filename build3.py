cd1c6f58b2fa97cb5075db07642f4eb889f86d61
twix20 <leereek@gmail.com>
2018-06-17 23:09:53 +0200

    python3 build

---
+++ build3.py
@@ -0,0 +1,18 @@
+#clean previous build
+rm -rf build fibonacci dist *.egg-info __pycache__
+rm -f *.pyd *.spec
+
+if [[ $* == --clean ]]; then
+    exit 0
+fi
+
+#build extension
+python3 setup_ext.py build_ext --inplace
+
+#create module
+mkdir --parents ./fibonacci
+mv fibonacci.*.pyd ./fibonacci
+touch ./fibonacci/__init__.py
+
+#dist wheel
+python3 setup.py sdist bdist_wheel
